2cc459a8a5eccff871fb077bdcc46e52a78f4fc3
wiwilin <katherine.linw@gmail.com>
2019-09-02 23:34:46 +1000

    File change

--- src/ClientConnection.java
+++ src/ClientConnection.java
@@ -64,4 +64,5 @@ public class ClientConnection {
         //output.flush();
 
     }
+
 }
--- src/ConnectionThread.java
+++ src/ConnectionThread.java
@@ -9,30 +9,55 @@ public class ConnectionThread implements Runnable {
     private DataOutputStream output;
     private int counter;
     public ServerGUI serverGUI;
+    public DicOperate dicOperate;
 
 
-    public ConnectionThread(Socket clientsocket,int counter,ServerGUI serverGUI) {
+    public ConnectionThread(Socket clientsocket, int counter, ServerGUI serverGUI) {
         this.clientsocket = clientsocket;
-        this.counter=counter;
-        this.serverGUI=serverGUI;
+        this.counter = counter;
+        this.serverGUI = serverGUI;
+
     }
 
     @Override
     public void run() {
 
-        try{
+        try {
             input = new DataInputStream(clientsocket.getInputStream());
 
             output = new DataOutputStream(clientsocket.getOutputStream());
 
-            while(true) {
-                serverGUI.text_log.append(input.readUTF());
-                output.writeUTF("reply by server:Hi Client"+counter+"!\n");
+            while (true) {
+                String str = input.readUTF();
+                serverGUI.text_log.append(str);
+                executeCommand(str);
+                // output.writeUTF("reply by server:Hi Client"+counter+"!\n");
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+
+    public synchronized void executeCommand(String command) throws IOException {
+        String[] str = command.split(",");
+        switch (str[0]) {
+            case "search":
+                output.writeUTF(dicOperate.searchDic(str[1]));
+                break;
+            case "add":
+                dicOperate.addDic(str[1], str[2]);
+                break;
+            case "delete":
+                dicOperate.deleteDic(str[1]);
+                break;
+            case "kill":
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + str[0]);
+        }
+    }
+
 
 }
